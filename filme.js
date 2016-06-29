12525cb13830917da59b175610f17bd7b80df92f
Lou Klappenbach <Lou Klappenbach>
2016-06-29 08:13:48 +0200

    änderungen der indext.html wegen tooltip plakat war zu groß. und .js fehler korrektur datatables

--- filme.js
+++ filme.js
@@ -1,5 +1,5 @@
 $("#filmtable").DataTable({
-    "ajax" : "data/filmdaten.json",
+    "ajax" : "filmdaten.json",
     "aLengthMenu": [5, 10, 25, 50, 75, "All"],
     "iDisplayLength": 5,
     "aaSorting": [1,'desc']
